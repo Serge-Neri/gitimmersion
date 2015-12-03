662c176254319c6c5e96e1f9701a35fceb024e58
serge_neri <saneri@uncc.edu>
2015-12-03 03:16:35 +0000

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,7 @@
-#Default is "World"
-#Author: Serge Neri (Ser@email.co)
+require 'greeter'
+
+# Default is World
 name = ARGV.first || "World"
-puts "Hello, #{name}!"
+
+greeter = Greeter.new(name)
+puts greeter.greet
